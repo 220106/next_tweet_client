85fd31527a3453eed2c4509efefb154cc4403d44
220106 <220106rq@yse-c.net>
2024-01-17 11:55:40 +0900

    update

--- app/auth/regist/page.tsx
+++ app/auth/regist/page.tsx
@@ -1,9 +1,21 @@
 "use client"
 
+import { FaUser } from "react-icons/fa";
+import Input from "@/app/components/Input";
+
 const RegistPage = () => {
     return (
-        <div>
-            Enter
+        <div className="mx-auto w-1/3">
+            <h1 className="flex text-2xl text-center font-bold">
+                <FaUser className="mt-1 me-3" />
+                Register
+            </h1>
+
+            <div>
+                <Input />
+                <Input />
+                <Input />
+            </div>
         </div>
     );
 }
